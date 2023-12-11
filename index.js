5281be5df8ebfabd94a84507445edec8dc974dfe
25Dark <henrydanso6@gmail.com>
2023-12-11 16:03:24 +0000

    protecting specific routes

--- index.js
+++ index.js
@@ -2,7 +2,16 @@ const express = require("express");
 const morgan = require("morgan");
 const app = express();
 
-app.use(morgan("dev"));
+app.use(morgan("common"));
+
+// protecting routes with fake middleware
+const verifyyPassword = (req, res, next) => {
+  const { password } = req.query;
+  if (password === "chickennugget") {
+    next();
+  }
+  res.send("sorry you need a password");
+};
 
 app.get("/", (req, res) => {
   res.send("HOME PAGE");
@@ -11,6 +20,12 @@ app.get("/", (req, res) => {
 app.get("/dogs", (req, res) => {
   res.send("WOOF WOOF");
 });
+
+// this route is protected
+app.get("/secret", verifyyPassword, (req, res) => {
+  res.send("I hate programing but i cant stop");
+});
+
 app.listen(3000, () => {
   console.log("APP LISTENING ON PORT 3000");
 });
